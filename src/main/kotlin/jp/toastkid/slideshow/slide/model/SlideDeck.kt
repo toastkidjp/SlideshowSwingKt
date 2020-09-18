4ea0afa2f9c3667e8aa25abce8f668ec8956e971
Toast Kid <super_q1m@yahoo.co.jp>
2020-09-19 07:15:05 +0900

    feature Implement bottom components.

--- src/main/kotlin/jp/toastkid/slideshow/slide/model/SlideDeck.kt
+++ src/main/kotlin/jp/toastkid/slideshow/slide/model/SlideDeck.kt
@@ -1,8 +1,14 @@
 package jp.toastkid.slideshow.slide.model
 
+import java.awt.BorderLayout
 import java.awt.CardLayout
+import javax.swing.BoxLayout
 import javax.swing.JFrame
+import javax.swing.JLabel
 import javax.swing.JPanel
+import javax.swing.JProgressBar
+import kotlin.math.max
+import kotlin.math.min
 
 class SlideDeck {
 
@@ -10,10 +16,17 @@ class SlideDeck {
 
     private val cards = CardLayout()
 
-    private var footerText: String = ""
+    private val bottomComponents = JPanel()
+
+    private val footerText = JLabel()
+
+    private val progress = JProgressBar(JProgressBar.HORIZONTAL, 0, 100)
 
     init {
         cardPanel.layout = cards
+        bottomComponents.layout = BoxLayout(bottomComponents, BoxLayout.Y_AXIS)
+        bottomComponents.add(footerText)
+        bottomComponents.add(progress)
     }
 
     fun add(slide: Slide) {
@@ -23,25 +36,33 @@ class SlideDeck {
 
     fun first() {
         cards.first(cardPanel)
+        progress.value = 1
     }
 
     fun back() {
         cards.previous(cardPanel)
+        progress.value = max(1, progress.value - 1)
     }
 
     fun forward() {
         cards.next(cardPanel)
+        progress.value = min(cardPanel.componentCount, progress.value + 1)
     }
 
     fun setTo(frame: JFrame) {
-        frame.add(cardPanel)
+        progress.maximum = cardPanel.componentCount
+        val content = JPanel()
+        content.layout = BorderLayout()
+        content.add(cardPanel, BorderLayout.CENTER)
+        content.add(bottomComponents, BorderLayout.SOUTH)
+        frame.add(content)
     }
 
     fun setFooterText(footerText: String?) {
         if (footerText.isNullOrBlank()) {
             return
         }
-        this.footerText = footerText
+        this.footerText.text = footerText
     }
 
 }
